92afb62eb081ebae93db6c55f4821608ee1cd303
javier monasterio <javiermonasterio@gmail.com>
2022-04-09 00:06:24 +0200

    testing HeapSort - try in __test__ folder

--- src/algorithms/sorting/__test__/Sort.test.js
+++ src/algorithms/sorting/__test__/Sort.test.js
@@ -1,5 +1,6 @@
 import Sort from '../bubble-sort/BubbleSort.js';
 import BubbleSort from '../bubble-sort/BubbleSort.js';
+import HeapSort from '../heap-sort/HeapSort.js';
 
 //Declare some random arrays
 export const sortedArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
@@ -9,11 +10,17 @@ export const equalArr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 export const negativeArr = [-1, 0, 5, -10, 20, 13, -7, 3, 2, -3];
 export const negativeArrSorted = [-10, -7, -3, -1, 0, 2, 3, 5, 13, 20];
 
-console.log(reverseArr);
+//log the original array
+console.log('Original array...')
+console.log(notSortedArr.toString());
 
-const sorter = new BubbleSort();
+//Heap Sort
+console.log('Performing HeapSort')
+const HeapSorter = new HeapSort();
+console.log(HeapSorter.sort(notSortedArr).toString());
 
 //We now sort the array
-console.log('Sorting...');
-
+const sorter = new BubbleSort();
 console.log(sorter.sort(reverseArr));
+
+
--- src/algorithms/sorting/heap-sort/HeapSort.js
+++ src/algorithms/sorting/heap-sort/HeapSort.js
@@ -1,5 +1,26 @@
-import Sort from '../Sort';
-import MinHeap from '../../../data-structures/heap/MinHeap';
+/**
+ * Heap Sort
+ * heapsort is a comparison-based sorting algorithm. Heapsort can be thought of as an improved selection sort: 
+ * like selection sort, heapsort divides its input into a sorted and an unsorted region, and it iteratively shrinks 
+ * the unsorted region by extracting the largest element from it and inserting it into the sorted region. 
+ * Unlike selection sort, heapsort does not waste time with a linear-time scan of the unsorted region; rather, 
+ * heap sort maintains the unsorted region in a heap data structure to more quickly find the largest element 
+ * in each step.
+ * 
+ * Although somewhat slower in practice on most machines than a well-implemented quicksort, it has the advantage 
+ * of a more favorable worst-case O(n log n) runtime. Heapsort is an in-place algorithm, but it is not a stable sort.
+ * 
+ * Heapsort was invented by J. W. J. Williams in 1964.
+ * This was also the birth of the heap, presented already by Williams as a useful data structure in its own right.
+ * In the same year, R. W. Floyd published an improved version that could sort an array in-place, 
+ * continuing his earlier research into the treesort algorithm.
+ * 
+ * [https://en.wikipedia.org/wiki/Heapsort]
+ * 
+ * 
+ */
+import Sort from '../Sort.js';
+import MinHeap from '../../../data-structures/heap/MinHeap.js';
 
 export default class HeapSort extends Sort {
   sort(originalArray) {
@@ -24,7 +45,6 @@ export default class HeapSort extends Sort {
 
       sortedArray.push(nextMinElement);
     }
-
     return sortedArray;
   }
 }
